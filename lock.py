6b1ec451be76ee3de14a1af884a148bee32a6b51
hp685 <Hjdp1988*>
2018-10-05 15:33:34 -0700

    threaded consumer

--- lock.py
+++ lock.py
@@ -1,10 +1,10 @@
 import uuid
+import threading
 from kombu import Exchange
 from kombu import Connection
 from kombu import Producer
 from kombu import Consumer
 from kombu import Queue
-from consume import serve_request
 
 
 _DEFAULT_LOCK_EXCHANGE = Exchange('_DEFAULT_LOCK_EXCHANGE')
@@ -19,16 +19,17 @@ _REQUEST_Q = Queue(
 
 class DistLock(object):
 
-    _lock_acquired_by = None
+    producers = dict()
+
 
     def __init__(self):
         self.requester = Producer(
             Connection(),
             _DEFAULT_LOCK_EXCHANGE,
             auto_declare=True,
-            routing_key=_ROUTING_KEY,
         )
         self.id = str(uuid.uuid4())
+        DistLock.producers[self.id] = self.requester
 
     def acquire(self):
         self.requester.publish(
@@ -53,12 +54,28 @@ class DistLock(object):
         self.release()
 
 
+def serve_request(message):
+   print message
+   message.ack()
+
+
 def setup_consumer():
     _lock_manager = Consumer(
         Connection(),
         queues=[_REQUEST_Q],
         on_message=serve_request,
-
     )
     _lock_manager.add_queue(_REQUEST_Q)
-    _lock_manager.consume()
+    _lock_manager.consume(no_ack=True)
+    while True:
+        try:
+            _lock_manager.connection.drain_events()
+        except (KeyboardInterrupt, SystemExit):
+            break
+
+
+def consume_messages():
+    t = threading.Thread(target=setup_consumer)
+    t.start()
+
+
