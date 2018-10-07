a7b9d65a23aedbba44b6bad899ffdcb5b59dc855
hp685 <Hjdp1988*>
2018-10-07 01:08:24 -0700

    threaded consumer

--- lock.py
+++ lock.py
@@ -1,5 +1,6 @@
 import uuid
 import threading
+import redis
 from kombu import Exchange
 from kombu import Connection
 from kombu import Producer
@@ -15,12 +16,17 @@ _REQUEST_Q = Queue(
     routing_key=_ROUTING_KEY,
     channel=Connection(),
 )
+_LOCK_REQUEST_Q = Queue(
+    name='LOCK_REQUESTS_QUEUE',
+    exchange=_DEFAULT_LOCK_EXCHANGE,
+    routing_key='LOCK_REQUESTS',
+    channel=Connection(),
+)
 
 
 class DistLock(object):
 
     producers = dict()
-
 
     def __init__(self):
         self.requester = Producer(
@@ -29,7 +35,19 @@ class DistLock(object):
             auto_declare=True,
         )
         self.id = str(uuid.uuid4())
-        DistLock.producers[self.id] = self.requester
+        self.consumer_q = Queue(name=self.id, exchange=_DEFAULT_LOCK_EXCHANGE)
+        self.consumer = Consumer(
+            Connection(),
+            on_message=self.read_response,
+            queues=[self.consumer_q],
+        )
+
+        self.red_connection = redis.StrictRedis()
+        self.listen_thread = threading.Thread(target=self.listener)
+        self.listen_thread.daemon = True
+        self.hold_lock = threading.Event()
+        self.listen_thread.start()
+        DistLock.producers[self.id] = (self.requester, self.consumer)
 
     def acquire(self):
         self.requester.publish(
@@ -38,6 +56,8 @@ class DistLock(object):
             exchange=_DEFAULT_LOCK_EXCHANGE,
             routing_key=_ROUTING_KEY,
         )
+        # Block until acknowledgement from broker
+        #self.hold_lock.wait()
 
     def release(self):
         self.requester.publish(
@@ -46,6 +66,19 @@ class DistLock(object):
             exchange=_DEFAULT_LOCK_EXCHANGE,
             routing_key=_ROUTING_KEY
         )
+        # clear hold event
+        self.hold_lock.clear()
+
+    def listener(self):
+        while self.red_connection.get('is_consuming'):
+            self.consumer.connection.drain_events()
+
+        self.listen_thread.join()
+        # Purge all other requests if we are exiting?
+
+    def read_response(self, message):
+        print(message)
+        self.hold_lock.set()
 
     def __enter__(self):
         self.acquire()
@@ -53,30 +86,69 @@ class DistLock(object):
     def __exit__(self):
         self.release()
 
+    def __del__(self):
+        self.requester.connection.release()
+        self.consumer.connection.release()
+        self.listen_thread.join()
+
 
 def serve_request(message):
+    print(message.payload)
+    p = Producer(Connection(),
+                _DEFAULT_LOCK_EXCHANGE,
+                auto_declare=True,
+                )
+    if message.payload.request == 'RELEASE':
 
-   print message
-   message.ack()
+        p.publish(
+            dict(request='OK')
+        )
+    else:
+        p.publish(
+            dict(request='ENQUEUE', id=message.id),
+            routing_key='QUEUE_REQUEST'
+
+        )
 
 
 def setup_consumer():
-    _lock_manager = Consumer(
-        Connection(),
-        queues=[_REQUEST_Q],
-        on_message=serve_request,
-    )
-    _lock_manager.add_queue(_REQUEST_Q)
-    _lock_manager.consume(no_ack=True)
-    while True:
-        try:
-            _lock_manager.connection.drain_events()
-        except (KeyboardInterrupt, SystemExit):
-            break
+    red = redis.StrictRedis()
+    red.set('is_consuming', True)
+    consumer_thread.start()
 
 
 def consume_messages():
-    t = threading.Thread(target=setup_consumer)
-    t.start()
+    red = redis.StrictRedis()
+    _lock_manager.add_queue(_REQUEST_Q)
+    _lock_manager.consume(no_ack=True)
 
+    while red.get('is_consuming'):
+        try:
+            _lock_manager.connection.drain_events(timeout=1)
+        except:
+            pass
+
+
+def stop_consumer():
+    red = redis.StrictRedis()
+    red.delete('is_consuming')
+    consumer_thread.join()
+    _lock_manager.connection.release()
+    print('consumer stopped')
+
+
+_lock_manager = Consumer(
+    Connection(),
+    queues=[_REQUEST_Q],
+    on_message=serve_request,
+)
+
+lock_request_consumer = Consumer(
+    Connection(),
+    queues=[_LOCK_REQUEST_Q],
+    #on_message=
+)
+
+consumer_thread = threading.Thread(target=consume_messages)
+consumer_thread.daemon = True
 
